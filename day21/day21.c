3a72d576ba5f1e570a5b771d83a600c4a39da73b
joltdx <jorgen.lindqvist@joltdx.se>
2024-12-26 11:09:33 +0100

    day 21

--- day21/day21.c
+++ day21/day21.c
@@ -21,6 +21,61 @@ void read_codes(const char *filename) {
 }
 
 
+typedef struct Node {
+    char keys[32];
+    int num;
+    long count;
+    struct Node *next;
+} Node;
+
+#define HASH_SIZE 997
+Node *hashTable[HASH_SIZE] = {NULL};
+
+int hash(const char *str, int num) {
+    // djb2 of string + number
+    unsigned long hash = 5381;
+    int c;
+    while ((c = *str++))
+        hash = ((hash << 5) + hash) + c;
+    hash = ((hash << 5) + hash) + num;
+    return hash % HASH_SIZE;
+}
+
+int hashInsertCount = 0;
+
+void hashInsert(char *keys, int num, long count) {
+    int index = hash(keys, num);
+    Node *newNode = (Node*)malloc(sizeof(Node));
+    strcpy(newNode->keys, keys);
+    newNode->next = hashTable[index];
+    newNode->num = num;
+    newNode->count = count;
+    hashTable[index] = newNode;
+}
+
+Node *hashGet(char *keys, int num) {
+    int index = hash(keys, num);
+    Node *current = hashTable[index];
+    while(current != NULL) {
+        if (current->num == num && strcmp(current->keys, keys) == 0) {
+            return current;
+        }
+        current = current->next;
+    }
+    return NULL;
+}
+
+void freeHashTable() {
+    for (int i = 0; i < HASH_SIZE; i++) {
+        Node *current = hashTable[i];
+        while (current != NULL) {
+            Node *next = current->next;
+            free(current);
+            current = next;
+        }
+    }
+}
+
 typedef struct {
     int x;
     int y;
@@ -29,9 +84,9 @@ typedef struct {
 const XY KPD1[11] = {{1,3},{0,2},{1,2},{2,2},{0,1},{1,1},{2,1},{0,0},{1,0},{2,0},{2,3}};
 XY kpd1_pos = {2,3};
 
-char rpd_out[25][5] = {{0}};
-char rpd_prev[25] = {' '};
-char rpd_pos[25] = {'A'};
+char rpd_out[26][5] = {{0}};
+char rpd_prev[26] = {' '};
+char rpd_pos[26] = {'A'};
 
 char *robot_pad(char c, int n) {
     if (c == rpd_prev[n]) {
@@ -41,7 +96,7 @@ char *robot_pad(char c, int n) {
         if (rpd_pos[n] == 'v')
             strcpy(rpd_out[n], "^A");
         else if (rpd_pos[n] == '<')
-            strcpy(rpd_out[n], ">^A");
+            strcpy(rpd_out[n], ">^A");     
         else if (rpd_pos[n] == '>')
             strcpy(rpd_out[n], "<^A");
         else if (rpd_pos[n] == 'A')
@@ -55,7 +110,7 @@ char *robot_pad(char c, int n) {
         else if (rpd_pos[n] == '>')
             strcpy(rpd_out[n], "<A");
         else if (rpd_pos[n] == 'A')
-            strcpy(rpd_out[n], "v<A");
+            strcpy(rpd_out[n], "<vA");     
     }
     else if (c == '<') {
         if (rpd_pos[n] == '^')
@@ -81,7 +136,7 @@ char *robot_pad(char c, int n) {
         if (rpd_pos[n] == '^')
             strcpy(rpd_out[n], ">A");
         else if (rpd_pos[n] == 'v')
-            strcpy(rpd_out[n], ">^A");
+            strcpy(rpd_out[n], "^>A"); 
         else if (rpd_pos[n] == '<')
             strcpy(rpd_out[n], ">>^A");
         else if (rpd_pos[n] == '>')
@@ -180,44 +235,54 @@ void generate_keypad_1(int from, int to, char keys[2][16]) {
     }
 }
 
-
-void generate_keypad_2(char *seq, char keys[2][32]) {
-
-    memset(keys, 0, 2 * 32 * sizeof(char));
-
+void generate_robot_pad(char *seq, char *keys, int n) {
+    memset(keys, 0, 64 * sizeof(char));
     int pos = 0;
     char c;
     int i = 0;
     while (c = seq[i++]) {
-        //printf("%s", keypad_2(c));
-        //strcpy(keys[0]+pos, keypad_2(c));
-        strcpy(keys[0]+pos, robot_pad(c,2));
-        pos = strlen(keys[0]);
-    }
-}
-
-void generate_keypad_4(char *seq, char keys[2][32]) {
-
-    memset(keys, 0, 2 * 32 * sizeof(char));
-
-    int pos = 0;
-    char c;
-    int i = 0;
-    while (c = seq[i++]) {
-        //printf("%s", keypad_2(c));
-        //strcpy(keys[0]+pos, keypad_3(c));
-        //char *blah = robot_pad(c,4);
-        strcpy(keys[0]+pos, robot_pad(c,3));
-        pos = strlen(keys[0]);
+        strcpy(keys+pos, robot_pad(c,n));
+        pos = strlen(keys);
     }
 }
 
 void init_stuff() {
     kpd1_pos = (XY){2,3};
-    for(int i = 0; i < 25; i++) {
+    for(int i = 0; i < 26; i++) {
         rpd_prev[i] = ' ';
         rpd_pos[i] = 'A';
     }
+}
+
+long robot_pad_pressing(char *seq, int num) {
+    if (num == 0) {
+        return(strlen(seq));
+    }
+
+    Node *memoized = hashGet(seq, num);
+    if (memoized != NULL) {
+        return memoized->count;
+    }
+
+    char next_seq[64] = {0};
+    char pad_seq[64] = {0};
+    char go_seq[64] = {0};
+
+    long sum = 0;
+    int pos = 0;
+    for (int n = 0; seq[n] != '\0'; n++) {
+        if (seq[n] == 'A') {
+            strncpy(go_seq, seq+pos, n+1-pos);
+            go_seq[n+1-pos] = '\0';
+            generate_robot_pad(go_seq, next_seq, num);
+            sum += robot_pad_pressing(next_seq, num - 1);
+            pos = n + 1;
+        }
+    }
+
+    hashInsert(seq, num, sum);
+
+    return sum;
 }
 
 int part_1( ) {
@@ -228,38 +293,63 @@ int part_1( ) {
         init_stuff();
 
         char keys[2][16] = {0};
-        char keys_2[2][32] = {0}; 
-        char keys_3[2][32] = {0};
+        char keys_2[64] = {0}; 
+        char keys_3[64] = {0};
         int p = 0;
         int c;
         int f = 10;
         int length = 0;
         while (c = pin[p++]) {
-            int len1 = INT_MAX;
-            int len2 = INT_MAX;
+            long len1 = LONG_MAX;
+            long len2 = LONG_MAX;
             int t = c == 'A' ? 10 : c - '0';
             generate_keypad_1(f, t, keys);
             if (keys[0][0] != '\0') {
-                generate_keypad_2(keys[0], keys_2);
-                generate_keypad_4(keys_2[0], keys_3);
-                //printf("%2d -> %2d: %s - %s - %s\n", f, t, keys[0], keys_2[0], keys_3[0]);
-                len1 = strlen(keys_3[0]);
+                len1 = robot_pad_pressing(keys[0], 2);
             }
             if (keys[1][0] != '\0') {
-                generate_keypad_2(keys[1], keys_2);
-                generate_keypad_4(keys_2[0], keys_3);
-                //printf("%2d -> %2d: %s - %s - %s\n", f, t, keys[1], keys_2[0], keys_3[0]);
-                len2 = strlen(keys_3[0]);
+                len2 = robot_pad_pressing(keys[1], 2);
             }
             f = t;
             length += len2 < len1 ? len2 : len1;
         }
         int complexity = atoi(pin);
         sum += complexity * length;
-    //printf("%s: %d * %d = %d\n", pin, complexity, length, complexity * length);
-        //char keys_2[2][16];
-        //generate_keypad_2("^^A", keys_2);
-        //printf("%s\n", keys_2[0]);
+    }
+
+    return sum;
+}
+
+long part_2( ) {
+    long sum = 0;
+    
+    for (int i = 0; i < 5; i++) {
+        char *pin = codes[i];
+        init_stuff();
+
+        char keys[2][16] = {0};
+        char keys_2[64] = {0}; 
+        char keys_3[64] = {0};
+        int p = 0;
+        int c;
+        int f = 10;
+        long length = 0;
+        while (c = pin[p++]) {
+            long len1 = LONG_MAX;
+            long len2 = LONG_MAX;
+            int t = c == 'A' ? 10 : c - '0';
+            generate_keypad_1(f, t, keys);
+            if (keys[0][0] != '\0') {
+                len1 = robot_pad_pressing(keys[0], 25);
+            }
+            if (keys[1][0] != '\0') {
+                len2 = robot_pad_pressing(keys[1], 25);
+            }
+            f = t;
+            length += len2 < len1 ? len2 : len1;
+        }
+        int complexity = atoi(pin);
+        sum += complexity * length;
     }
 
     return sum;
@@ -278,6 +368,10 @@ int main(int argc, char **argv) {
 
     int result1 = part_1();
     printf("part 1: %d\n", result1);
+    long result2 = part_2();
+    printf("part 2: %ld\n", result2);
+
+    freeHashTable();
 
     return 0;
 }
